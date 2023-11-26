18ece9a28111ea2c5dd5839c2bfc181c7a675ada
hirosukt <senolynn7584@gmail.com>
2023-11-26 16:48:02 +0900

    fix decimal point of imhere coord

--- src/main/kotlin/love/chihuyu/lifeutilities/command/commands/ImhereCommand.kt
+++ src/main/kotlin/love/chihuyu/lifeutilities/command/commands/ImhereCommand.kt
@@ -10,7 +10,7 @@ object ImhereCommand : Command {
         commandAPICommand("imhere") {
             withPermission("lifeutilities.imhere")
             playerExecutor { player, _ ->
-                player.server.broadcast(Component.text("${player.name}の現在地: X:${player.location.x}, Y: ${player.location.y}, Z: ${player.location.z}"))
+                player.server.broadcast(Component.text("${player.name}の現在地: X:${"%.1f".format(player.location.x)}, Y: ${"%.1f".format(player.location.y)}, Z: ${"%.1f".format(player.location.z)}"))
             }
         }
     }
